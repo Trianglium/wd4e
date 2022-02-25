ff609365c9ce7defc12fdd751630b7bb65b6090a
Jordan Brocker Rudow <Jordan.Rudow@gmail.com>
2022-02-25 11:40:26 -0600

    Finish auto_complete JS

--- C3_JS/js/auto_complete.js
+++ C3_JS/js/auto_complete.js
@@ -1,1 +1,15 @@
 /*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
+function billingFunction(){
+  var sName = document.getElementById('shippingName');
+  var sZip = document.getElementById('shippingZip');
+  var bName = document.getElementById('billingName');
+  var bZip = document.getElementById('billingZip');
+  var chkBox = document.getElementById('same');
+  if (chkBox.checked === true) {
+    bName.value = sName.value;
+    bZip.value = sZip.value;
+  } else {
+    bName.value = '';
+    bZip.value = '';
+  }
+}
