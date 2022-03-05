d6752c8816b5f48cb5bcd4ea39bee487c3ae8f63
Trianglium <jbrockerrudow@gmail.com>
2022-03-05 11:44:22 -0600

    Global template update

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class AboutView(TemplateView):
-    template_name = "templates/about.html"
+    template_name = "about.html"
 
 class HomeView(TemplateView):
-    template_name = "templates/home.html"
+    template_name = "home.html"
 
 class ContactView(TemplateView):
-    template_name = "templates/contact.html"
+    template_name = "contact.html"
