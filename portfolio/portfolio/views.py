8f0cbeb2a4a49dd61a7a3d5b3b9a7eed2165460b
Trianglium <jbrockerrudow@gmail.com>
2022-03-05 12:49:25 -0600

    Update to reflect relocation

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class AboutView(TemplateView):
-    template_name = "about.html"
+    template_name = "portfolio/about.html"
 
 class HomeView(TemplateView):
-    template_name = "home.html"
+    template_name = "portfolio/home.html"
 
 class ContactView(TemplateView):
-    template_name = "contact.html"
+    template_name = "portfolio/contact.html"
