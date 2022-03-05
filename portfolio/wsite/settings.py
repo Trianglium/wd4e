f311c0a25599bfddeccd81ba3622e55583783481
Trianglium <jbrockerrudow@gmail.com>
2022-03-05 12:48:58 -0600

    Updated for static

--- portfolio/wsite/settings.py
+++ portfolio/wsite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'portfolio.apps.PortfolioConfig',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/STATIC_URL]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
