d3608c175ce133352b995ee2f5d4d711b2d9d593
Trianglium <jbrockerrudow@gmail.com>
2022-03-06 08:46:30 -0600

    Context data

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -5,11 +5,31 @@ from django.views.generic.base import TemplateView
 class AboutView(TemplateView):
     template_name = "portfolio/about.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['title'] = 'About'
+        return context
+
 class HomeView(TemplateView):
     template_name = "portfolio/home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['title'] = 'Home'
+        return context
 
 class ContactView(TemplateView):
     template_name = "portfolio/contact.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        context['title'] = 'Contact'
+        return context
+
 class ProjectsView(TemplateView):
     template_name = "portfolio/projects.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectsView, self).get_context_data(**kwargs)
+        context['title'] = 'Projects'
+        return context
