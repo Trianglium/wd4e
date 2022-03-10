7e993978bb2c1c64853c0f1b9905b1c3c0ef01e1
Trianglium <jbrockerrudow@gmail.com>
2022-03-10 08:53:36 -0600

    CBV Updates for new templates

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -10,26 +10,9 @@ class AboutView(TemplateView):
         context['title'] = 'About'
         return context
 
-class HomeView(TemplateView):
-    template_name = "portfolio/home.html"
-    
+class IndexView(TemplateView):
+    template_name = "portfolio/index.html"
     def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['title'] = 'Home'
-        return context
-
-class ContactView(TemplateView):
-    template_name = "portfolio/contact.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(ContactView, self).get_context_data(**kwargs)
-        context['title'] = 'Contact'
-        return context
-
-class ProjectsView(TemplateView):
-    template_name = "portfolio/projects.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(ProjectsView, self).get_context_data(**kwargs)
-        context['title'] = 'Projects'
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = 'Index'
         return context
